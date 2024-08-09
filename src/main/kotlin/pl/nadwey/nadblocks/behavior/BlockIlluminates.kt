91bd58e36813f02fdf6876ad2480007bbcac2672
Nadwey <nadweyyy@gmail.com>
2024-08-09 09:49:29 +0200

    Improve BlockIlluminates behavior

--- src/main/kotlin/pl/nadwey/nadblocks/behavior/BlockIlluminates.kt
+++ src/main/kotlin/pl/nadwey/nadblocks/behavior/BlockIlluminates.kt
@@ -15,8 +15,9 @@ import net.minecraft.core.BlockPos as MojangPos
 
 // https://www.spigotmc.org/threads/1-19-3-1-20-3-nms-make-block-emit-light.594267/
 class BlockIlluminates(private val level: Int) : BlockBehavior {
+
     companion object {
-        val LIGHT_BLOCK_FACES: Array<Direction> = arrayOf<Direction>(
+        val LIGHT_BLOCK_FACES = arrayOf(
             Direction.DOWN,
             Direction.EAST,
             Direction.NORTH,
@@ -35,30 +36,27 @@ class BlockIlluminates(private val level: Int) : BlockBehavior {
     }
 
     override fun handleBreak(pos: BlockPos, state: NovaBlockState, ctx: Context<DefaultContextIntentions.BlockBreak>) {
-        val serverLevel: ServerLevel = pos.world.serverLevel
+        val serverLevel = pos.world.serverLevel
 
-        for (face in LIGHT_BLOCK_FACES) {
-            val entry: MojangPos = pos.nmsPos.relative(face)
-            val blockData: BlockState = serverLevel.getBlockState(entry)
-            if (blockData.isAir || blockData.block === Blocks.LIGHT) {
+        LIGHT_BLOCK_FACES.forEach { face ->
+            val entry = pos.nmsPos.relative(face)
+            val blockData = serverLevel.getBlockState(entry)
+            if (blockData.isAir || blockData.block == Blocks.LIGHT) {
                 serverLevel.destroyBlock(entry, false)
             }
         }
     }
 
     private fun updateLight(pos: BlockPos) {
-        val serverLevel: ServerLevel = pos.world.serverLevel
+        val serverLevel = pos.world.serverLevel
+        val newState = Blocks.LIGHT.defaultBlockState().setValue(LightBlock.LEVEL, level)
 
-        var newState: BlockState = Blocks.LIGHT.defaultBlockState()
-        newState = newState.setValue(LightBlock.LEVEL, level)
-
-        for (face in LIGHT_BLOCK_FACES) {
-            val entry: MojangPos = pos.nmsPos.relative(face)
-            val blockData: BlockState = serverLevel.getBlockState(entry)
-            if (blockData.isAir || blockData.block === Blocks.LIGHT) {
+        LIGHT_BLOCK_FACES.forEach { face ->
+            val entry = pos.nmsPos.relative(face)
+            val blockData = serverLevel.getBlockState(entry)
+            if (blockData.isAir || blockData.block == Blocks.LIGHT) {
                 serverLevel.setBlock(entry, newState, 2, 0)
             }
         }
     }
-
 }
